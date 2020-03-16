baa9617a477cf9479d1e0942e29dd9b47479d090
rahat-chy <rht345@gmail.com>
2020-03-17 01:40:32 +0600

    Initial Commit

--- app/src/main/java/com/example/a180104067offline2/MainActivity.java
+++ app/src/main/java/com/example/a180104067offline2/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.a180104067offline2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button button1,button2,button3,button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+        button4 = (Button) findViewById(R.id.button4);
+
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if(v.getId()==R.id.button1)
+        {
+            Intent intent = new Intent(MainActivity.this,symptoms.class);
+            startActivity(intent);
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/a180104067offline2/symptoms.java
@@ -0,0 +1,31 @@
+package com.example.a180104067offline2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class symptoms extends AppCompatActivity implements View.OnClickListener{
+
+    Button button1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symptoms);
+
+        button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        Intent intent = new Intent(symptoms.this,MainActivity.class);
+        startActivity(intent);
+
+    }
+}
